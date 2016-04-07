4e1f2c6a670664d584c848532e03cdaeea02339e
aasmundolav <aasmundolav@users.noreply.github.com>
2016-04-07 14:08:20 +0200

    Update and rename Arcpy Layers Various to Arcpy Layers Various.py

--- Arcpy Layers Various.py
+++ Arcpy Layers Various.py
@@ -1,3 +1,15 @@
+# Replace text in layer names
+mxd = arcpy.mapping.MapDocument("CURRENT")
+df = mxd.activeDataFrame
+layers=arcpy.mapping.ListLayers(mxd, "*", df)
+replace = "Ana_EarthByte"
+
+for l in layers:
+    print l.name
+    if replacetext in l.name:
+        print l.name
+
+#---------------------------------
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = mxd.activeDataFrame
 layers=arcpy.mapping.ListLayers(mxd, "Pipeline Labels", df)
@@ -5,7 +17,7 @@ layers=arcpy.mapping.ListLayers(mxd, "Pipeline Labels", df)
 for l in layers:
     for x in l.labelClasses:
         print x.definitonQuery
----------------
+#---------------
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = mxd.activeDataFrame
@@ -16,7 +28,7 @@ for l in layers:
         x.SQLQuery = a[count]
         count+=1
 
--------------------
+#-------------------
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = mxd.activeDataFrame
@@ -32,7 +44,7 @@ for l in layers:
                 finfo.setVisible(x,"HIDDEN") 
         arcpy.MakeFeatureLayer_management(l,"New_Installations","1=1",r"d:\MarinfWebkart2\MarinInf_1.gdb\MarinInf_1.gdb",finfo)
         
---------------
+#--------------
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = mxd.activeDataFrame
