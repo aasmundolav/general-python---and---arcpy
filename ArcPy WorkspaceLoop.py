4e561234ee1af61c2b82bad27fb0b4de5190e5ee
aasmundolav <aasmundolav@users.noreply.github.com>
2016-05-10 09:42:04 +0200

    Update ArcPy WorkspaceLoop.py

--- ArcPy WorkspaceLoop.py
+++ ArcPy WorkspaceLoop.py
@@ -4,9 +4,14 @@ arcpy.env.workspace = r"c:\Users\aoek\AppData\Roaming\Rothwell\PaleoGIS\Scratch"
 workspaces = arcpy.ListWorkspaces("*", "FileGDB")
 
 for workspace in workspaces:
-  arcpy.env.workspace = workspace
-  featureclasses = arcpy.ListFeatureClasses("World*")
-  for featureclass in featureclasses:
-    arcpy.AddField_management(featureclass, "TIME_SLIDER", "TEXT")
-    arcpy.CalculateField_management(featureclass, "TIME_SLIDER", os.path.split(workspace)[1])
-    arcpy.Merge_management(featureclass, r"d:\worldBasemap\Paleomap.gdb\World")
+    print os.path.split(workspace)[1]
+    arcpy.env.workspace = workspace
+    featureclasses = arcpy.ListFeatureClasses("World*")
+    for featureclass in featureclasses:
+        try:
+            arcpy.AddField_management(featureclass, "TIME_SLIDER", "TEXT")
+        except:
+            print "add field failed.."
+        arcpy.CalculateField_management(featureclass, "TIME_SLIDER", "'%s'" % os.path.split(workspace)[1][0:10],"PYTHON_9.3") ##
+        arcpy.Append_management(featureclass, r"d:\worldBasemap\Paleomap.gdb\World","NO_TEST")
+        break
