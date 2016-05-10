31837dccc426394d96d5f1a465f9ed32b65f8877
aasmundolav <aasmundolav@users.noreply.github.com>
2016-05-10 09:41:44 +0200

    Update ArcPy WorkspaceLoop.py

--- ArcPy WorkspaceLoop.py
+++ ArcPy WorkspaceLoop.py
@@ -1,11 +1,12 @@
 import arcpy, os.path
 
-arcpy.env.workspace = "c:/data"
+arcpy.env.workspace = r"c:\Users\aoek\AppData\Roaming\Rothwell\PaleoGIS\Scratch"
 workspaces = arcpy.ListWorkspaces("*", "FileGDB")
 
 for workspace in workspaces:
   arcpy.env.workspace = workspace
-  featureclasses = arcpy.ListFeatureClasses("*")
+  featureclasses = arcpy.ListFeatureClasses("World*")
   for featureclass in featureclasses:
     arcpy.AddField_management(featureclass, "TIME_SLIDER", "TEXT")
     arcpy.CalculateField_management(featureclass, "TIME_SLIDER", os.path.split(workspace)[1])
+    arcpy.Merge_management(featureclass, r"d:\worldBasemap\Paleomap.gdb\World")
