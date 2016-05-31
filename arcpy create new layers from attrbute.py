becb0db392593c674146bc7b93b4b190ddfcabc1
aasmundolav <aasmundolav@users.noreply.github.com>
2016-05-31 15:46:39 +0200

    Create arcpy create new layers from attrbute.py

---
+++ arcpy create new layers from attrbute.py
@@ -0,0 +1,15 @@
+field = "AGE_BASE_NAME"
+mxd = arcpy.mapping.MapDocument("Current")
+df = arcpy.mapping.ListDataFrames(mxd)[0]
+layer = 'Source Rocks, SOMA'
+LC = arcpy.mapping.ListLayers(mxd,layer,df)
+cursor = arcpy.SearchCursor(layer)
+att= []
+ 
+for c in cursor:
+ 	if not c.getValue(field) in att:
+ 		att.append(c.getValue(field))
+ 		CL = LC[0]
+ 		CL.name = "%s - %s" % (c.getValue(field),layer)
+ 		CL.definitionQuery = "%s = '%s'" % (field,c.getValue(field))
+ 		arcpy.mapping.AddLayer(df,CL,"TOP")
