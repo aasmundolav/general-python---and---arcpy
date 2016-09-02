a1883dfadd7135deb1e9dec0c0070d4622ec2124
aasmundolav <aasmundolav@users.noreply.github.com>
2016-09-02 15:11:26 +0200

    Create arcpy replace data sources.py

---
+++ arcpy replace data sources.py
@@ -0,0 +1,18 @@
+mxd = arcpy.mapping.MapDocument("CURRENT")
+for l in arcpy.mapping.ListLayers(mxd,"*",mxd.activeDataFrame):
+	print l.datasetName
+	print l.name
+	
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+for l in arcpy.mapping.ListLayers(mxd,"*",mxd.activeDataFrame):
+	if ".RIG_ANCHOR" in l.datasetName:
+		l.replaceDataSource(l.workspacePath, "SDE_WORKSPACE", l.datasetName.replace(".RIG_ANCHOR",".WGS84_RIG_ANCHOR"), False)
+		print l.datasetName
+
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+for l in arcpy.mapping.ListLayers(mxd,"*",mxd.activeDataFrame):
+	if "ED50" in l.datasetName:
+		l.replaceDataSource(l.workspacePath, "SDE_WORKSPACE", l.datasetName.replace("ED50_","WGS84_"), False)
+		print l.datasetName
