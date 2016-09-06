9a930a11024d318ee9aa777d65dc7608c829d867
aasmundolav <aasmundolav@users.noreply.github.com>
2016-09-06 15:42:28 +0200

    Update arcpy replace data sources.py

--- arcpy replace data sources.py
+++ arcpy replace data sources.py
@@ -16,3 +16,11 @@ for l in arcpy.mapping.ListLayers(mxd,"*",mxd.activeDataFrame):
 	if "ED50" in l.datasetName:
 		l.replaceDataSource(l.workspacePath, "SDE_WORKSPACE", l.datasetName.replace("ED50_","WGS84_"), False)
 		print l.datasetName
+
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+for l in arcpy.mapping.ListLayers(mxd,"*",mxd.activeDataFrame):
+        if l.isBroken and ".sde" not in l.dataSource:
+            print "%s;%s" % (l.longName, l.dataSource)
+            
+            
