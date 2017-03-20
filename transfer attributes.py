b086c704f9a7889d88bf79a9396cf969019d8eee
aasmundolav <aasmundolav@users.noreply.github.com>
2017-03-20 13:55:25 +0100

    Update transfer attributes.py

--- transfer attributes.py
+++ transfer attributes.py
@@ -1,6 +1,6 @@
 with arcpy.da.UpdateCursor('TEST', ['SHAPE@', 'SURVEY_ID_REF']) as cursor:
     for row in cursor:
-        iGeometry = arcpy.da.SearchCursor('All Surveys', ['SHAPE@','SURVEY_ID'],) SURVEY_ID = row[1]
+        iGeometry = arcpy.da.SearchCursor('All Surveys', ['SHAPE@','SURVEY_ID'],where_clause="SURVEY_ID = '%s'" % row[1]
         for g in iGeometry:
             if g[1] == row[1]:
                 print "match %s" % g[1]
