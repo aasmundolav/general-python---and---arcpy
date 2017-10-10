25837fad60bc1092b1381cedd9446291cd3291af
aasmundolav <aasmundolav@users.noreply.github.com>
2017-10-10 14:24:52 +0200

    Create Show geometry in label expression.py

---
+++ Show geometry in label expression.py
@@ -0,0 +1,9 @@
+def FindLabel ( [OBJECTID] ):
+  mxd = arcpy.mapping.MapDocument("CURRENT")
+  layers=arcpy.mapping.ListLayers(mxd, "test")
+  lr=layers[0]
+  with arcpy.da.SearchCursor(lr, 'Shape@','OBJECTID = '+str( [OBJECTID] )) as cursor:
+      for row in cursor:
+         a=row[0].lastPoint.X
+         b=row[0].lastPoint.Y
+         return'easting:{:.0f}\nnorthing:{:.0f}'.format(a,b)
