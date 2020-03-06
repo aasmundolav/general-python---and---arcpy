be4ac853a7311e511b62e521e408a36c52340275
aasmundolav <aasmundolav@users.noreply.github.com>
2020-03-06 09:59:22 +0100

    Create _formatPythonCode.py

---
+++ _formatPythonCode.py
@@ -0,0 +1,14 @@
+message1="New ascii DTM files have not been placed in DTM_ASCII and DTM_ESRI folder"
+## FOLDER is in FROM CONTRACTOR
+## FILES has _DTM_ in filename.
+## File suffix xyz. files may be zipped.
+## Data has not been loaded.
+
+message2="New ascii DTM files have been placed in DTM_ASCII folder, but no files are located in DTM_ESRI folder\nPlease convert files to DTM_ESRI format"
+## FOLDER IS DTM_ASCII
+## Data has not been loaded.
+
+message3="New DTM files are located in DTM_ASCII and DTM_ESRI folder. Please load data in Raster database"
+## FOLDER IS IN DTM_ESRI
+## Data has not been loaded.
+## Avoid loading pre-files for SRI?
