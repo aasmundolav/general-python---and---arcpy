a4fb518677307ad8702c2d1ef74829fb7905ce7d
aasmundolav <aasmundolav@users.noreply.github.com>
2021-09-30 15:33:06 +0200

    Update removeValuesFromASCII.py

--- GDALXYZ/removeValuesFromASCII.py
+++ GDALXYZ/removeValuesFromASCII.py
@@ -1,2 +1,5 @@
 import pandas as pd
-df = pd.read_csv(f
+df = pd.read_csv(r"c:\temp\y.xyz", sep=" ", names=["X","Y","Z"])
+print(df.head())               
+df = df.query('Z < 10000')
+df.to_csv(r"c:\temp\x.xyz", sep=' ', header=None, index=None)
