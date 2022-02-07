8e92dd034b53d60294b2044ec8a0285b58061900
aasmundolav <aasmundolav@users.noreply.github.com>
2022-02-07 20:43:10 +0100

    Update arcpy_printPDFwithMultipleDQ.py

--- arcpy_printPDFwithMultipleDQ.py
+++ arcpy_printPDFwithMultipleDQ.py
@@ -1,14 +1,17 @@
 Years = range(2010,2022)
 mxd = arcpy.mapping.MapDocument("CURRENT")
+df = mxd.activeDataFrame
 
 for y in Years:
+	print y
 	layers=arcpy.mapping.ListLayers(mxd, "*", df)
-	for elm in arcpy.mapping.ListLayoutElements(mxd, "GRAPHIC_ELEMENT"):
-		if elm.name == "Title":
-			elm.text = """<BOL>Sporing Fiskeri Tampen %s</BOL>
-Data fra Fiskeridirektoratet 2012-2021""" % y
-
+	print y
+	aa=arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT")[0]
+	
+	aa.text = """<BOL>Sporing Fiskeri Tampen %s</BOL>
+	Data fra Fiskeridirektoratet 2010-2020""" % y
+	
 	for l in layers:
 		if "Fiskeri" in l.name:
 			l.definitionQuery = "Year = %s" % y
-	arcpy.mapping.ExportToPDF(mxd, r"C:\temp\FDIR%s.pdf" % y)
+	arcpy.mapping.ExportToPNG(mxd, r"C:\temp\FDIR%s.png" % y)
