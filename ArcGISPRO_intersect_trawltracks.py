c3035fcfc30cc7477fd11f9a3c07e20b6c3eac1e
aasmundolav <aasmundolav@users.noreply.github.com>
2022-03-11 12:38:02 +0100

    Update ArcGISPRO_intersect_trawltracks.py

--- ArcGISPRO_intersect_trawltracks.py
+++ ArcGISPRO_intersect_trawltracks.py
@@ -4,11 +4,11 @@ l = m.listLayers()[0]
 
 for c in cursor:
     count +=1
+    print(count)
     print(c.NAME)
-    l.definitionQuery = "NAME = '{0}'".format(c.NAME)
     arcpy.management.SelectLayerByAttribute('pipelines', 'NEW_SELECTION',"NAME = '{0}'".format(c.NAME))
     print("pipeline feature selected")
     arcpy.management.SelectLayerByLocation('tracks', 'INTERSECT', 'pipelines')
     print("track features selected")
-    arcpy.analysis.Intersect("pipelines #;tracks #", r"D:\Fish_DNVGL\Pipelines.gdb\A{0}".format(count), 'ALL',None, 'POINT')
+    arcpy.analysis.Intersect("pipelines #;tracks #", r"D:\Fish_DNVGL\Pipelines.gdb\B{0}".format(count), 'ALL',None, 'POINT')
     print("intersect completed")
