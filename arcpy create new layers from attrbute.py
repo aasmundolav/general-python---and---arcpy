e49e2d0ec5bf555bf4250a7761f2e767e318c916
aasmundolav <aasmundolav@users.noreply.github.com>
2022-03-16 10:25:41 +0100

    Update arcpy create new layers from attrbute.py

--- arcpy create new layers from attrbute.py
+++ arcpy create new layers from attrbute.py
@@ -1,7 +1,7 @@
 field = "NAME"
 mxd = arcpy.mapping.MapDocument("Current")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
-layer = 'Source Rocks, SOMA'
+layer = 'ThisOne'
 LC = arcpy.mapping.ListLayers(mxd,layer,df)
 cursor = arcpy.SearchCursor(layer)
 att= []
@@ -10,6 +10,7 @@ for c in cursor:
  	if not c.getValue(field) in att:
  		att.append(c.getValue(field))
  		CL = LC[0]
- 		CL.name = "%s - %s" % (c.getValue(field),layer)
+ 		CL.name = "%s" % c.getValue(field)
  		CL.definitionQuery = "%s = '%s'" % (field,c.getValue(field))
  		arcpy.mapping.AddLayer(df,CL,"TOP")
+   arcpy.CopyFeatures_management(CL, r"\\ws630\Projects\PROJECT_SERVICES\UtsiraNord\Data\singleshapes2\%s.shp")
