fdb0b8f6b6357f73ed292242474e7ddd6e5aaab0
aasmundolav <aasmundolav@users.noreply.github.com>
2023-02-10 14:36:20 +0100

    Update Show geometry in label expression.py

--- Show geometry in label expression.py
+++ Show geometry in label expression.py
@@ -5,6 +5,6 @@ def FindLabel ( [OBJECTID] ):
   with arcpy.da.SearchCursor(lr, 'Shape@','OBJECTID = '+str( [OBJECTID] )) as cursor:
       for row in cursor:
          newSR = arcpy.SpatialReference(32631)
-         a=row[0].lastPoint.X
-         b=row[0].lastPoint.Y
+         a=row[0].projectAs(newSR)‍.lastPoint.X
+         b=row[0].projectAs(newSR)‍.lastPoint.Y
          return'easting:{:.0f}\nnorthing:{:.0f}'.format(a,b)
